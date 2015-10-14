5cfee7047a8a93c1dff0ef937110ae9de0ab497d
Peter Lightbody <pet1330@gmail.com>
2015-10-14 17:36:23 +0100

    Made Main a class

--- PIDControl/PIDController.py
+++ PIDControl/PIDController.py
@@ -12,37 +12,46 @@ import time                                                                    #
 from Modbus import comClient                                                   #Import Modbus Comms Class
 from PID import PIDController as controller
 
-		
-if __name__ == '__main__':
-    #Initialise Modbus comms class    
-    rw = comClient()
-    #Initialise PID Controller
-    ctrl = controller()
+        
+class researchProject:
     
-    #Main Method
-    while(True):
-        #For controller time loop
-        startTime = time.time()
-        
-        #Read Controller data as r (Mock in this case)
-        r = [199,0,0,356]
-        
-        #Send data to controller
-        u = ctrl.runCtrl(r)
-        
-        #Write output to valve
-        #Replace with 'rw.writeData(u)' when using real world comms
-        print u
-                
-        time.sleep(ctrl.dT - (time.time() - startTime))
-        
-        
-#Add Back In To Enable Real World Comms        
-"""        
-        #Read controller data        
-        try:
-            r = rw.getData()        
-        except:
-		print "Modbus Error: Connection Failed"
-		break
-"""
+    def __init__():
+        #Initialise Modbus comms class    
+        self.rw = comClient()
+        #Initialise PID Controller
+        self.ctrl = controller()
+
+    def run():
+        #Main Method
+        while(True):
+            #For controller time loop
+            startTime = time.time()
+            
+            #Read Controller data as r (Mock in this case)
+            r = [199,0,0,356]
+            
+            #Send data to controller
+            u = self.ctrl.runCtrl(r)
+            
+            #Write output to valve
+            #Replace with 'self.rw.writeData(u)' when using real world comms
+            print u
+                    
+            time.sleep(self.ctrl.dT - (time.time() - startTime))
+            
+            
+            #Add Back In To Enable Real World Comms        
+            """        
+            #Read controller data        
+            try:
+                r = self.rw.getData()        
+            except:
+            print "Modbus Error: Connection Failed"
+            break
+            """
+
+def main():
+    rp = researchProject()
+    rp.run()
+
+if __name__ == '__main__':main()
