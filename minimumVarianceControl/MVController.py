f42dfe3fb45efcf43e007cef019a74b20caf5b79
Alex <alex.leech@talktalk.net>
2015-11-04 10:44:11 +0000

    Update
    Rushing!

---
+++ minimumVarianceControl/MVController.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Author: Alexander David Leech
+Date:   14/10/2015
+Rev:    1
+Lang:   Python 2.7
+Deps:   Pyserial, Pymodbus
+Desc:   Main file for PID Controller
+"""
+
+import time                                 #Time based functions
+import numpy as np                          #Import numpy for array & matrices
+
+
+class researchProjectOnlineMVC:
+
+    def __init__(self):
+        print "Do something"
+
+    def run(self):
+        print "Do something"
