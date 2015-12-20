1673604c28a9a67794f6c8c3f3d1f4f48da518c7
FlaminMad <alex.leech@talktalk.net>
2015-12-20 12:54:11 +0000

    Updated plot graph file. Contains bug that needs fixing!

--- dev/plotOutput.py
+++ dev/plotOutput.py
@@ -2,45 +2,61 @@
 """
 Created on Thu Dec 17 11:06:16 2015
 
-@author: Toshiba
+@author: Alex Leech
 """
-import time
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 class plotActiveGraph:
     
     def __init__(self):
-        self.xdata = np.array([])
-        self.ydata = np.array([])
+        self.xdata = np.array([[]])
+        self.startFlag = 0
         self.fig = plt.subplots()
         plt.subplot(1,1,1)
         plt.ylim(0,1000)
-        plt.xlim(-20,20)
         plt.ion()
         plt.xlabel("Time (s)")
         plt.show()
 
-    def dataUpdate(self,x,y):
+    def dataUpdate(self,x,*plotData):       
+        # Update x axis data array        
         self.xdata = np.append(self.xdata,x)
-        self.ydata = np.append(self.ydata,y)
+        
+        if self.startFlag == 0:
+            # Initialise array for no of pens to plot
+            self.ydata = np.array([[]]).reshape(len(plotData),0)
+            self.ydata = np.append(self.ydata,np.transpose(np.matrix(plotData)),1)
+            self.startFlag = 1
+        else:
+            # Update y axis data matrix
+            self.ydata = np.append(self.ydata,np.transpose(np.matrix(plotData)),1)
+                        
+        #Correct array size for data length
+        if len(self.xdata) > 20: #Set this num up in json to allow variable length graphs
+            self.xdata = np.delete(self.xdata,0)
+            self.ydata = np.delete(self.ydata,0,1)            
+        
+        # Update graph
         self.plot()
         
     def plot(self):
-        plt.plot(self.xdata,self.ydata,'g-') # Adds data to your graph
-        plt.draw() # Draws the graph to your screen
+        #Adds data ready to write        
+        try:
+            for i in range (0, (len(self.ydata[:,0])-1)):
+                plt.plot(self.xdata,self.ydata[i,:],'g-',label='i')
+        except IndexError:
+            print ("Check ALL variables passed are setup in plotPenConf")
         
-        
-def func(x):
-    y = (np.square(x)+(2*x)+12)
-    return y
+        # Draws the graph to your screen
+        plt.draw()
+        # Workaround to avoid the freezing problem
+        plt.pause(0.001)
 
 def main():
-    pA = plotActiveGraph()
-    
-    for x in range(-20, 20):
-        pA.dataUpdate(x,func(x))
-        
-    plt.show(block=True)
-    
-if __name__ == '__main__': main()
+    pag = plotActiveGraph()
+    pag.dataUpdate(1,2,3,4)
+    pag.dataUpdate(2,3,4,5)
+
+if __name__ == '__main__':main()        
