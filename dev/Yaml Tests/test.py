7f87d30d76e659f291d06572d69fde19ff7562d5
Alex <alex.leech@talktalk.net>
2016-01-07 23:21:07 +0000

    07/01/2016

---
+++ dev/Yaml Tests/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Author: Alexander David Leech
+Date:   07/01/2016
+Rev:    1
+Lang:   Python 2.7
+Deps:   numpy, matplotlib, warnings
+"""
+
+import yaml
+
+class testYAML:
+    
+    def __init__(self):
+        print('ready')
+
+    def readFile(self):       
+        print('loading')
