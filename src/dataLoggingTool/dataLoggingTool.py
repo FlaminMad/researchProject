365ccbd4d6e9e226a7bdf72e759aabd1c1073e81
Alex <alex.leech@talktalk.net>
2016-01-18 22:11:03 +0000

    First Working Simulation

--- src/dataLoggingTool/dataLoggingTool.py
+++ src/dataLoggingTool/dataLoggingTool.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Author: Alexander David Leech
-Date:   14/10/2015
-Rev:    1
-Lang:   Python 2.7
-Deps:   Pyserial, Pymodbus
-Desc:   Main file for data logging
+@author: Alexander David Leech
+@date:   14/10/2015
+@rev:    1
+@lang:   Python 2.7
+@deps:   Pyserial, Pymodbus
+@desc:   Main file for data logging
 """
 
 import time                                                                    #Time based functions
 from Modbus import comClient                                                   #Import Modbus Comms Class
 from xlsLogging4Vars import xlsLogging4Vars
 from plotActiveGraph import plotActiveGraph
+from testModel import testModel
         
 class dataLoggingTool:
     
@@ -20,13 +21,15 @@ class dataLoggingTool:
         #Initialise Modbus comms class    
         self.rw = comClient()
         #Initialise excel data logging
-#        self.xls = xlsLogging4Vars()
+        self.xls = xlsLogging4Vars()
         #For Time Interval
         self.Interval = 5      
         #For 'heart beat' counter
         self.count = 0
         #For graphical plot
         self.pg = plotActiveGraph()
+        #For simulated lab rig
+        self.r = testModel()
 
     def run(self):
         #Main Method
@@ -36,17 +39,19 @@ class dataLoggingTool:
             
             #Read controller data as r     
             try:
-                r = self.rw.readData()        
+#                r = self.rw.readData()
+                self.r.readModel()
             except:
                 print "Modbus Error: Read Connection Failed"
                 break
 
             #Pass data to excel for logging purposes
-#            self.xls.writeXls(r)
+            self.xls.writeXls(self.r)
             
             #Add data to plot
-            self.pg.dataUpdate((time.time() - self.xls.startTime),r.getRegister(0),r.getRegister(2),r.getRegister(3))
-
+#            self.pg.dataUpdate((time.time() - self.xls.startTime),r.getRegister(0),r.getRegister(2),r.getRegister(3))
+            self.pg.dataUpdate((time.time() - self.xls.startTime),self.r.getRegister(0),self.r.getRegister(2),self.r.getRegister(3))
+            
             print self.count
             self.count += 1
              
