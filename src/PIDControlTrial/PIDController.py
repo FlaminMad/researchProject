c3847a698de6bd7f250f0386d67a9983124e5bcb
FlaminMad <alex.leech@talktalk.net>
2016-02-24 01:02:35 +0000

    PID Trial controller containing tests on 'bumping' spErr when the controller sign changes have been added to this dev branch. Note: Data to analyse if its possible is contained but the controller itself has not yet been modified.

---
+++ src/PIDControlTrial/PIDController.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Author: Alexander David Leech
+Date:   14/10/2015
+Rev:    2
+Lang:   Python 2.7
+Deps:   None
+Desc:   Contains PID controller class for import
+"""
+
+import numpy as np
+
+class PIDController:
+     
+     #Adjustable Controller Parameters
+     Kg = 1.32
+     Ki = 86.812
+     Kd = 13.095
+     dT = 5             #Time Interval
+     antiWindUp = 0.2   #Anti-Windup (Between 0.05 & 0.25)
+
+     #Setup Fixed Controller Variables
+     lowLimit = 0       # Valve low limit
+     highLimit = 1000   # Valve high limit
+     startupFlag = True    # For smooth transitioning
+     ctrlType = "PID"
+     
+     
+     def runCtrl(self,pv,sp,op):
+         #Calculate error at current time
+         error = sp - pv
+         
+         #To help reduce 'bump' in controller changeover
+         if self.startupFlag is True:
+             self.__transition(error,pv,op)
+             self.startupFlag = False
+         
+         #Run main PID algorithm based on selected type
+         if self.ctrlType == "PI":
+             u = self.Kg*(error + self.__integral(error))
+         elif self.ctrlType == "PID":
+             u = self.Kg*(error + self.__integral(error) + self.__derivitive(pv))
+         else:
+             raise ValueError('Invalid Control Type - Options are PI & PID')
+             
+         #Enforce Valve Limitations and antiwindup
+         if u > self.highLimit:
+             self.spErr += (self.antiWindUp * error)
+             print "High Alarm"
+             return 1000
+             
+         elif u < self.lowLimit:
+             self.spErr += (self.antiWindUp * error)
+             print "Low Alarm"
+             return 0
+             
+         else:
+             self.spErr += error
+             return u
+
+
+     def __integral(self,err):
+         ui = ((self.spErr *self.dT)/self.Ki)
+         return ui
+         
+     def __derivitive(self,pv):
+         #Note: Derivitive takes into account setpoint change spikes by using
+         #      the PV instead of error.
+         ud = (((self.deriv - pv) * self.Kd)/self.dT)
+         self.deriv = pv
+         return ud
+
+     def __transition(self,err,pv,u):  
+        # Calculate values for deriv and setErr to ensure seamless bump during
+        # controller changover.
+        self.deriv = pv
+        
+        #PI Control - Calc sp error rounded to 0 d.p
+        if self.ctrlType == "PI":
+            self.spErr = np.around(((self.Ki/self.dT)*((u/self.Kg)-err)),0)
+            
+        elif self.ctrlType == "PID":
+            self.spErr = np.around((self.Ki/self.dT)*((u/self.Kg)-err-((self.deriv*self.Kd)/self.dT)),0)
+        
+        else:
+            raise ValueError('Invalid Control Type - Options are PI & PID')
---
+++ src/PIDControlTrial/researchProjectPID.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Author: Alexander David Leech
+Date:   14/10/2015
+Rev:    1
+Lang:   Python 2.7
+Deps:   Pyserial, Pymodbus
+Desc:   Main file for PID Controller
+"""
+
+import time
+import sys ; sys.path.insert(0, '../dataLoggingTool')
+
+from osTools import osTools                 #For detecting exit keypress
+from xlsLogging import xlsLogging
+from plotActiveGraph import plotActiveGraph
+from PIDController import PIDController as controller
+
+if int(sys.argv[1]) == 1:
+    sys.path.insert(0, '../../tests')    
+    from testModel import testModel         #Import Simulation Class
+else:
+    from Modbus import comClient            #Import Modbus Comms Class
+
+        
+class researchProjectPID:
+        
+    def __init__(self):
+        # Objects
+        self.ext = osTools()            #Initialise data key press
+        self.xls = xlsLogging(6)        #Initialise excel data logging
+        self.pg = plotActiveGraph()     #Initialise graphical plot
+        self.ctrl = controller()        #Initialise PID Controller
+        if int(sys.argv[1]) == 1:
+            self.r = testModel("../../tests/")        #Initialise simulated lab rig
+        else:
+            self.rw = comClient()       #Initialise Modbus comms class
+
+        # Variables
+        self.count = 0                  #For 'heart beat' counter
+
+
+    def run(self):
+        startTime = time.time()                 #For time reference
+
+        while(True):
+            loopTime = time.time()              #Itteration start time
+            r = self.readDataPipe()             #Read data
+            
+            
+            self.pg.dataUpdate((time.time() - startTime),r.getRegister(0),r.getRegister(2),r.getRegister(3))    #Add data to plot
+            u = self.ctrl.runCtrl(r.getRegister(0),r.getRegister(2),r.getRegister(3))       #Run the controller
+            self.xls.writeXls(startTime,r,[self.ctrl.spErr,0])      #Log data in excel
+            
+            if int(sys.argv[1]) == 1:
+                self.r.writeModel(u)            #Write to model
+            else:
+                self.rw.dataHandler('w',u)      #Write to MODBUS system
+            
+            if self.ext.kbdExit():              #Detect exit condition
+                break
+            print self.count                    #Heartbeat
+            self.count += 1                     #Heartbeat
+            time.sleep(self.ctrl.dT - (time.time() - loopTime))
+    
+    
+    def readDataPipe(self):
+        if int(sys.argv[1]) == 1:
+            self.r.readModel()
+            return self.r
+        else:
+            return self.rw.dataHandler('r') 
+
+
+def main():
+    rp = researchProjectPID()
+    rp.run()
+    rp.pg.end()
+    
+if __name__ == '__main__':main()
