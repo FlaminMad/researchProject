4375dc84a2eb2124ffecdf25b08f95897437d132
FlaminMad <alex.leech@talktalk.net>
2016-03-05 15:52:09 +0000

    Sync Dev Branch

--- src/PIDControlTrial/PIDController.py
+++ src/PIDControlTrial/PIDController.py
@@ -18,46 +18,57 @@ class PIDController:
      Ki = 86.812
      Kd = 13.095
      dT = 5             #Time Interval
-     antiWindUp = 0.2   #Anti-Windup (Between 0.05 & 0.25)
+     ctrlType = "PI"
+     antiWindUp = 0.0   #Anti-Windup (Between 0.05 & 0.25)
 
-     #Setup Fixed Controller Variables
+     #Setup Valve Limitations
      lowLimit = 0       # Valve low limit
      highLimit = 1000   # Valve high limit
-     startupFlag = True    # For smooth transitioning
-     ctrlType = "PID"
      
+     
+     def __init__(self):
+         self.startupFlag = True        # For smooth transitioning
+         self.prevSP = 0                # For SP change detection
+         pass
      
      def runCtrl(self,pv,sp,op):
-         #Calculate error at current time
-         error = sp - pv
-         
-         #To help reduce 'bump' in controller changeover
-         if self.startupFlag is True:
-             self.__transition(error,pv,op)
-             self.startupFlag = False
-         
-         #Run main PID algorithm based on selected type
-         if self.ctrlType == "PI":
+         error = sp - pv                #Calculate error at current time
+         self.__tools(pv,sp,op,error)   #Antiwindup, Controller Transision &
+                                        #state change algorithms
+         if self.ctrlType == "PI":  
              u = self.Kg*(error + self.__integral(error))
          elif self.ctrlType == "PID":
              u = self.Kg*(error + self.__integral(error) + self.__derivitive(pv))
          else:
              raise ValueError('Invalid Control Type - Options are PI & PID')
+            
+         return self.__vlvLims(u,error)
+                  
+
+     def __tools(self,pv,sp,op,error):
+         
+         if self.startupFlag is True:                       #To help reduce 'bump' in controller changeover
+             self.deriv = pv
+             self.prevErr = error
+             self.__transition(pv,op,error)
+             print " Startup Flag"
+             self.startupFlag = False
              
-         #Enforce Valve Limitations and antiwindup
-         if u > self.highLimit:
-             self.spErr += (self.antiWindUp * error)
-             print "High Alarm"
-             return 1000
-             
-         elif u < self.lowLimit:
-             self.spErr += (self.antiWindUp * error)
-             print "Low Alarm"
-             return 0
-             
-         else:
-             self.spErr += error
-             return u
+         if sp - self.prevSP != 0:                          #Testing Methods
+             print " Set Point Change"
+             self.setPointChange = True
+        
+         if self.setPointChange is True:
+             print "Waiting..."
+             if np.sign(error)!= np.sign(self.prevErr):     #Test for sign change
+                 print "Sign Change"                 
+                 #self.__transition(pv,op,error)             #Re-calculate spErr
+                 print self.spErr
+                 self.setPointChange = False                #Reset variable
+         
+         self.prevErr = error
+         self.prevSP = sp
+         return
 
 
      def __integral(self,err):
@@ -71,17 +82,32 @@ class PIDController:
          self.deriv = pv
          return ud
 
-     def __transition(self,err,pv,u):  
+     def __transition(self,pv,u,error):  
         # Calculate values for deriv and setErr to ensure seamless bump during
-        # controller changover.
-        self.deriv = pv
-        
-        #PI Control - Calc sp error rounded to 0 d.p
+        # controller changover.        
+        # PI Control - Calc sp error rounded to 0 d.p
         if self.ctrlType == "PI":
-            self.spErr = np.around(((self.Ki/self.dT)*((u/self.Kg)-err)),0)
+            self.spErr = np.around(((self.Ki/self.dT)*((u/self.Kg)-error)),0)
             
         elif self.ctrlType == "PID":
-            self.spErr = np.around((self.Ki/self.dT)*((u/self.Kg)-err-((self.deriv*self.Kd)/self.dT)),0)
+            self.spErr = np.around((self.Ki/self.dT)*((u/self.Kg)-error-((self.deriv*self.Kd)/self.dT)),0)
         
         else:
             raise ValueError('Invalid Control Type - Options are PI & PID')
+
+
+     def __vlvLims(self,u,error):
+         #Enforce Valve Limitations and antiwindup
+         if u > self.highLimit:
+             self.spErr += (self.antiWindUp * error)
+             print "High Alarm"
+             return 100
+             
+         elif u < self.lowLimit:
+             self.spErr += (self.antiWindUp * error)
+             print "Low Alarm"
+             return 0
+             
+         else:
+             self.spErr += error
+             return u     
