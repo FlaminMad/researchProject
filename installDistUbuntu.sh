3ccadb2a924a0d70287a4d295672fdebb7439637
Alex Leech <FlaminMad@users.noreply.github.com>
2016-04-10 23:34:50 +0100

    Update to install script

--- installDistUbuntu.sh
+++ installDistUbuntu.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-apt-get -S python-numpy python-matplotlib python-pyserial python-yaml python-pip
-python2 -m pip install pymodbus
+#!/bin/bash
+apt-get install python-numpy python-matplotlib python-yaml python-openpyxl python-pip
+python2 -m pip install pyserial pymodbus
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 python2 ./tests/TESTINSTALL.py
